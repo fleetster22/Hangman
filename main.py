560a3354bf4de1d482663da36922e4c1e061d9b7
fleetster22 <fleetster22@gmail.com>
2020-12-07 12:18:40 -0500

    game works and artwork added

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
 # Day 7 of 100 Days of Coding Challenge
 
-# Steps
-# Generate random word
-# Generate as many blanks as characters in the word
-# Ask the player to choose a letter
-# Is the letter in the word - Yes. Replace the blank with the letter. No. Draw a line. Have they run out of guesses- Yes- you lose. No-Ask the player to guess a letter
-# Are all blanks filled - No. Ask player to guess a letter. Yes_ You win
-# TODO remove the chosen word from the list
+# TODO clear function not working
+
 
 import random
-word_list = ["dedication", "unicorn", "papaya", "aerodynamic", "saddle"]
+from word_list import *
+from artwork import *
+from os import system, name
 
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+        # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+
+game_over = False
 chosen_word = random.choice(word_list)
-print(chosen_word)
+lives = 6
 display = []
-
+print(logo)
 for x in range(len(chosen_word)):
     display += "_"
-print(display)
+print(f"{''.join(display)}")
 
-guess = input("Guess a letter\n").lower()
+while not game_over:
+    guess = input("Guess a letter\n").lower()
 
+    clear()
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-print(display)
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
 
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"{guess} not in answer.")
+        print(f"You have {lives} guesses remaining")
 
+        if lives == 0:
+            print("You lose")
+            print(f"The answer is {chosen_word}")
+            game_over = True
+    print(f"{''.join(display)}")
+    if "_" not in display:
+        game_over = True
+        print("You win!")
+    print(stages[lives])
